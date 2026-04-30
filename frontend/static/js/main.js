dbd32657d4111a5071fbedda42916669c4d25760
verivlk <74357960+verivlk@users.noreply.github.com>
2026-04-30 16:23:23 +0100

    fix fav showing - rename beach id (#27)

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -711,7 +711,7 @@ buscarBtn.addEventListener("click", async () => {
         console.log("fav ids:", favoriteIds);  // TODO for debug
 
         data.resultados.forEach(playa => {
-            playa.isFavorite = favoriteIds.includes(playa.playa_id);
+            playa.isFavorite = favoriteIds.includes(playa.beach_id);
         });
         console.log("Resultados obtenidos:", data.resultados);  // TODO for debug
 
@@ -823,7 +823,7 @@ function pintarResultados(resultados) {
                 </div>
 
                 <div class="beach-summary-right">
-                    <button class="favorite-btn" data-id="${playa.playa_id}">
+                    <button class="favorite-btn" data-id="${playa.beach_id}">
                         ${playa.isFavorite ? '❤️' : '🤍'}
                     </button>
                     <div class="score-badge">Score: ${Number(playa.score).toFixed(1)}</div>
