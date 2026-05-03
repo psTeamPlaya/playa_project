41298a167eeaf82d02990dd858617c9c5ee59ae4
Erick Justo Sosa <erick101_is2@protonmail.com>
2026-05-03 13:33:20 +0100

    Database finished

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -4,3 +4,6 @@ from .auth import router as auth_router
 from .users import router as users_router
 # from .favourites import router as favourites_router
 from .services import router as services_router
+from .activities import router as activities_router
+from .variables import router as variables_router
+from .beach_conditions import router as beach_conditions_router
